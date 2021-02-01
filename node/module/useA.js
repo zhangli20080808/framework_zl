9f22b1e08890cea283435a0052a85f7fe4e94d24
zhangli20080808 <791492434@qq.com>
2021-02-01 22:10:59 +0800

    require理解 exports 和 module.exports 的关系

--- node/module/useA.js
+++ node/module/useA.js
@@ -2,9 +2,87 @@
  * node中可以使用同步的方式读取文件
  * 一调用 require 会把当前用户写的代码包装到一个函数中
  */
+const path = require('path')
+const fs = require('fs')
+const vm = require('vm')
 
-const result = require('./a');
-console.log(result);
+// console.log(result);
+
+function Module (id) {
+  this.id = id
+  this.exports = {}  // 最终模块的返回结果
+}
+
+Module._extensions = {
+  '.js' (module) {
+    console.log('加载js模块')
+    let content = fs.readFileSync(module.id, 'utf8')
+    content = Module.wrapper[0] + content + Module.wrapper[1]
+    // 需要让函数字符串变成真正的函数
+    let fn = vm.runInThisContext(content)
+    // console.log(fn.toString()) // function (exports, require, module,__filename, __dirname) {module.exports = 'zhangLi'}
+    let exports = module.exports // {}
+    let dirname = path.dirname(module.id) // /Users/zhangli/framework_zl/node/module
+    // 把包装的函数执行 require是会让包装的函数执行 并且会改变this
+    fn.call(exports, exports, req, module, dirname, module.id)
+  },
+  '.json' (module) {
+    let content = fs.readFileSync(module.id, 'utf8')
+    module.exports = JSON.parse(content)
+  }
+}
+
+Module.wrapper = [
+  `(function (exports, require, module,__filename, __dirname) {`
+  ,
+  `})`
+]
+
+Module._resolveFilename = function (filename) {
+  let absPath = path.resolve(__dirname, filename)
+  let exists = fs.existsSync(absPath)
+  if (exists) {
+    return absPath
+  }
+  let keys = Object.keys(Module._extensions)
+  for (let i = 0; i < keys.length; i++) {
+    let newPath = absPath + keys[i]
+    let flag = fs.existsSync(newPath)
+    if (flag) {
+      return newPath
+    }
+  }
+  throw new Error('模块不存在')
+}
+
+Module.prototype.load = function () {
+  let extname = path.extname(this.id)
+  Module._extensions[extname](this)  //
+}
+
+Module._cache = {}
+
+function req (filename) { // 默认传入的文件名可能是没有.js .json后缀的,如果没有 尝试添加
+  filename = Module._resolveFilename(filename) // /Users/zhangli/framework_zl/node/module/a.js
+  // 创建一个模块
+  // 加载前先看一眼 是否加载过了  如果加载过了 我们直接读缓存
+
+  let cacheModule = Module._cache[filename]  // 多次引用同一个模块只运行一次
+  if (cacheModule) {
+    return cacheModule.exports  //返回缓存的结果
+  }
+
+  let module = new Module(filename)
+  Module._cache[filename] = module
+  // 加载模块
+  module.load()
+  return module.exports
+}
+
+const result = req('./a')
+const result2 = req('./zl.json')
+// exports 和 module.exports 的关系  exports = module.exports  = {}
+console.log(result, result2)
 
 
 /**
@@ -17,11 +95,11 @@ console.log(result);
  * 6. Module._extensions 文件的处理方式
  * 7. 核心就是读取文件
  * 8. 给文件外层增加一个函数 并且让函数执行 改变了this，传入了  exports, require, module,filename, dirname属性
-   9. 用户会给  module.exports 赋值                            
+ 9. 用户会给  module.exports 赋值
  * 10.最终返回  module.exports 拿到最终结果
 
  简单理解-> 把文件读出来 包个函数 执行 传入对象 用户填值 填完返回
- * 
+ *
  */
 
 /**
@@ -33,12 +111,12 @@ console.log(result);
 }(exports, module, __filename, __dirname)
  */
 
- /**
-  * 
-  * @param {*} 
-  * 1. node --inspect-brk 
-  * chrome://inspect
-  */
+/**
+ *
+ * @param {*}
+ * 1. node --inspect-brk
+ * chrome://inspect
+ */
 
 // function sun(a) {
 //   return function (b) {
