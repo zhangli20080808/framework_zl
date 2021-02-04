711b133028321ad116e6942f66b26dd7808f792c
zhangli20080808 <791492434@qq.com>
2021-02-04 23:55:51 +0800

    sj

---
+++ node/module/2.core.js
@@ -0,0 +1,43 @@
+/**
+ * 1. fs
+ * 2. path
+ * 3. vm 做一个虚拟机运行环境
+ */
+const fs = require('fs')
+// readdirSync 同步api
+const path = require('path')
+
+const filePath = path.resolve(__dirname, '../../.gitignore')
+// 判断文件是否存在 不过已经被废弃掉了 返回布尔值
+fs.existsSync(filePath)  //true
+
+// 读取文件全部采用绝对路径
+// console.log(path.resolve(__dirname)) // /Users/zhangli/framework_zl/node/module
+fs.readFileSync(filePath, 'utf8')
+/**
+ * 如果需要拼接 / 必须使用join resolve会回到根目录下
+ * @type {string}
+ */
+let s = path.join('a', 'b', 'c')  // 拼接 可以拼接/
+let s2 = path.resolve(__dirname, 'a', 'b', 'c')
+console.log(s, s2)  // /Users/zhangli/framework_zl/node/module/a/b/c
+// 获取文件拓展名
+let ext = path.extname('a.min.js') // .js
+let basename = path.basename('a.min.ts', 'ts')
+console.log(ext, basename)
+
+/**
+ * 如何让一个字符串执行
+ * 1. eval  存在问题 模块间需要相互独立,不希望模块的变量共享
+ * 2. new Function 可以字符串转换为函数 再去调用 一般前端中让一个模块可以运行采用这种方式
+ * @type {module:vm}
+ */
+let vm = require('vm')
+let a = 100
+// let str = `console.log(a)` // 100
+
+let fn = new Function('a', 'b', `console.log(a); return 100`)
+console.log(fn(1,2))
+// console.log(fn.toString())
+
+
