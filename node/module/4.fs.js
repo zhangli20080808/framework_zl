0d3199e9a76c6c6efc27cfba97e9f1618cd0b4ff
zhangli20080808 <791492434@qq.com>
2021-02-07 21:20:52 +0800

    删除目录

--- node/module/4.fs.js
+++ node/module/4.fs.js
@@ -6,7 +6,6 @@
 // 实现拷贝
 const fs = require('fs')
 const path = require('path')
-console.log(path.resolve(__dirname))
 // 在代码运行期间最好使用异步 运行时使用同步可能会阻塞
 // 这种拷贝不适合大文件操作 比如视频文件 我们需要读一点写一点 采用流的方式
 // 如何区分文件大小呢？ 如果文件超过64k，就是用流的方式  小于64k直接 read write
@@ -41,7 +40,7 @@ function mkdirSync (path) {
   }
 }
 
-mkdirSync('a/b/c/d/e/f')
+// mkdirSync('a/b/c/d/e/f')
 
 // 异步实现的话 肯定是第一个创建完再创建第二个 next方法 递归创建 上来先调用一次
 
@@ -72,7 +71,47 @@ function mkdir (path, cb) {
   next()
 }
 
-mkdir('f/s/d/a/s', () => {
-  console.log('创建陈宫')
-})
+// mkdir('f/s/d/a/s', () => {
+//   console.log('创建陈宫')
+// })
+
+// 删除目录 1.得保证目录非空 先遍历到最底层 从先网上删除
+
+/**
+ * 删除目录思路 先把儿子删除掉 再删父亲
+ * 读取目录 readdirSync
+ * 删除目录 rmdirSync
+ * 删除文件 unlinkSync
+ * 判断状态 statSync
+ * @param p
+ */
+function rmdirSync (p) {
+  try {
+    // 判断文件路径是否存在
+    fs.accessSync(p)
+  } catch (e) {
+    return
+  }
+  let dirs = fs.readdirSync(p)  // [ '1.txt', 'a' ]
+  // 路径映射
+  dirs = dirs.map(dir => path.join(p, dir))  // [ 'q/1.txt', 'q/a' ]
+  console.log(dirs)
+  // 需要判断 这个路径 是文件  还是文件夹 在删除
+  dirs.forEach(item => {
+    // 判断文件状态 statSync
+    let statObj = fs.statSync(item)
+    if (statObj.isDirectory()) {
+      // isDirectory  isFile 不是目录就是文件
+      // console.log('目录', item)
+      fs.rmdirSync(item)  // 删除目录
+    } else {
+      // console.log('文件', item)
+      fs.unlinkSync(item)  // 删除文件
+    }
+  })
+  fs.rmdirSync(p)
+}
+
+rmdirSync('q')
+
 
