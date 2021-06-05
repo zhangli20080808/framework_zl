8e37b177fc2cce91afb3a6a64f8931ed29a3f012
zhangli20080808 <791492434@qq.com>
2021-06-05 16:37:51 +0800

    观察者模式

--- 架构知识promise/手写promise/4、观察者模式.js
+++ 架构知识promise/手写promise/4、观察者模式.js
@@ -1,6 +1,8 @@
-// 观察者模式 (基于发布订阅模式，而且观察者模式和发布订阅模式之间是有关联的) 两者没有直接关联
 
-//观察者  被观察者 (被观察者需要收集所有的观察者)  把观察者放到被观察者中 小宝宝不开心->通知爸爸妈妈
+/**
+ * 观察者模式 (基于发布订阅模式，而且观察者模式和发布订阅模式之间是有关联的) 两者没有直接关联
+ * 观察者  被观察者 (被观察者需要收集所有的观察者)  把观察者放到被观察者中 小宝宝不开心->通知爸爸妈妈
+ */
 
 class Subject {  //被观察者 小宝宝
   constructor (name) {
@@ -9,12 +11,14 @@ class Subject {  //被观察者 小宝宝
     this.state = '开心'
   }
 
+  // 注册观察者 基于发布订阅 存起来
   attach (o) {
     this.observers.push(o)
   }
 
   setState (newState) {
     this.state = newState
+    // 通知观察者我的状态发生了变化
     this.observers.forEach(o => o.update(this))
   }
 }
@@ -34,7 +38,7 @@ let baby = new Subject('宝宝')
 let o1 = new Observer('爸爸')
 let o2 = new Observer('妈妈')
 
-//把观察者 attach 注册进去
+//注册关系  把观察者 attach 注册进去 需要让观察者和被观察者产生管理
 baby.attach(o1)
 baby.attach(o2)
 baby.setState('不开心')  // on emit
