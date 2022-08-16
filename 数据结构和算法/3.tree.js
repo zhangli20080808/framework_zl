63e1357519f2e8bd3cdcb7601aad07e25accb6da
zhangli20080808 <791492434@qq.com>
2022-08-16 22:01:42 +0800

    bfs | dfs

--- 数据结构和算法/3.tree.js
+++ 数据结构和算法/3.tree.js
@@ -1,3 +1,75 @@
 /**
-
+深度优先遍历：尽可能深的搜索树的分支
+广度优先遍历：先访问距离根节点最近的节点
 */
+// dfs
+const treeData = {
+  val: 'a',
+  children: [
+    {
+      val: 'b',
+      children: [
+        {
+          val: 'd',
+          children: [],
+        },
+        {
+          val: 'e',
+          children: [],
+        },
+      ],
+    },
+    {
+      val: 'c',
+      children: [
+        {
+          val: 'f',
+          children: [],
+        },
+        {
+          val: 'g',
+          children: [],
+        },
+      ],
+    },
+  ],
+};
+
+/**
+ * 深度
+1. 访问根节点
+2. 对根节点的children挨个进行深度优先遍历
+ * @param {*} root 
+ */
+const dfs = (root) => {
+  console.log(root.val);
+  root.children.forEach(dfs);
+};
+console.log(dfs(treeData), 'con');
+// a
+// b
+// d
+// e
+// c
+// f
+// g
+// undefined con
+
+/**
+1. 新建一个队列，把根节点入队  -> 根节点 a，
+2. 把队头出队并访问，
+3. 把队头的children挨个入队
+4. 重复第二，第三步骤，直到队列为空
+ * @param {*} root 
+ */
+const bfs = (root) => {
+  const queue = [root];
+  while (queue.length > 0) {
+    const n = queue.shift();
+    console.log(n.val);
+    n.children.forEach((item) => {
+      queue.push(item);
+    });
+  }
+};
+bfs(treeData);
