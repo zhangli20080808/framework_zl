c2fa644cf174d448fc64da6e58087507747e1c79
zhangli20080808 <791492434@qq.com>
2024-11-24 15:06:50 +0800

    链表实现队列

--- 数据结构和算法/13. 使用js翻转单向链表.js
+++ 数据结构和算法/13. 使用js翻转单向链表.js
@@ -15,9 +15,11 @@ function createLinkList(arr = []) {
   // { value: 300 }
   // { value: 200 ,next: { value: 300 }}
   // { value: 100 ,next: { value: 200, next:{ value: 300 } }}
+
   let currentNode = {
     value: arr[arr.length - 1],
   };
+  // 倒着循环的方式
   for (let i = arr.length - 2; i >= 0; i--) {
     const cur = arr[i];
     currentNode = {
@@ -32,5 +34,3 @@ function createLinkList(arr = []) {
 const arrTest = [100, 200, 300];
 const ps = createLinkList(arrTest);
 console.log(ps);
-
-//
